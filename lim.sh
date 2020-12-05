b9a34bff43bb707a8ed70dbe6c4dba7681e5f5ba
blackestsaint <45312935+blackestsaint@users.noreply.github.com>
2020-12-05 23:37:23 +0800

    Update lim.sh

--- lim.sh
+++ lim.sh
@@ -2,7 +2,6 @@
 
 sleep 5
 
-screen -dm bash -c "./ohpserver -port 8899 -proxy 172.105.123.119:61790 -tunnel 172.105.123.119::22"
 
 chmod +x /etc/adm-lite/limiter.sh &
    /etc/adm-lite/limiter.sh &
