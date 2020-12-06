ca3e42451e4bdace7a560b9a958c604cbb49385b
blackestsaint <45312935+blackestsaint@users.noreply.github.com>
2020-12-06 10:19:59 +0800

    Update k.sh

--- k.sh
+++ k.sh
@@ -64,8 +64,12 @@ echo "@reboot root sudo su
 @reboot root /etc/adm-lite/ohp.sh
 @reboot root /bin/sleep 5 && sudo iptables-restore < /etc/iptables/rules.v4" >> /etc/crontab
 sleep 3
+cd
 echo "Port 333
 Port 334
 Port 335" >> /etc/ssh/sshd_config
+sleep 1
+cd
+echo -e " End of Installation | Type ADM | Created by Gwapong Lander "
 
 
