e86445bb809d4111f7c3fc2ecc9caee0ebeaddba
blackestsaint <45312935+blackestsaint@users.noreply.github.com>
2020-12-15 23:39:04 +0800

    Update m1.sh

--- m1.sh
+++ m1.sh
@@ -340,5 +340,5 @@ echo "   - Other concern and questions of these auto-scripts?" | tee -a log-inst
 echo "      Direct Messege : www.facebook.com/kornips" | tee -a log-install.txt | lolcat
 echo ""
 echo ""
-read -p " After you read installation log. Press Enter to "Restart" 
+read -p " After you read installation log. Press Enter to "Restart" "
 sudo reboot
